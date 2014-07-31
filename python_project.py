a6c0b3b03fc7407b781473569c7e3aee36664719
unknown <agmacd3842@GIS-VLAB4.ad.uauth.net>
2014-07-31 01:07:23 -0400

    https://github.com/agmacd3842/Python-Final.git

--- python_project.py
+++ python_project.py
@@ -7,25 +7,7 @@ arcpy.AddMessage(sys.argv)
 if sys.argv[1].split('.')[1] == 'gpx':
     env.overwriteoutput = True
     arcpy.GPXtoFeatures_conversion(sys.argv[1], sys.argv[2])
-##    arcpy.SelectLayerByAttribute_management('in_memory\newfile', 'NEW_SELECTION', "\"Type\" = 'TRKPT'")
-    arcpy.PointsToLine_management('newfile', sys.argv[2], 'Name', '#', 'NO_CLOSE')
 ###Convert KML to Layer
 elif sys.argv[1].split('.')[1] == 'kml':
-    outLocation == ["CURRENT"]
-    MasterGDB = ["CURRENT"]
-    MasterGDBLocation = ["CURRENT"]
-    arcpy.CreateFileGDB_management(outLocation, MasterGDB)
-    for kmz in arcpy.ListFiles('*.KM*'):
-        print "CONVERTING: " + OS.path.join(arcpy.env.workspace,kmz)
-        arcpy.KMLToLayer_conversion(kmz, outLocation)
-    arcpy.env.workspace = outLocation
-    wks = arcpy.ListWorkspaces('*' , 'FileGDB')
-    wks.remove(MasterGDBLocation)
-    for fgdb in wks:
-        arcpy.env.workspace = fgdb
-        featureClasses = arcpy.ListFeatureClasses('*', '', 'Placemarks')
-        for fc in featureClasses:
-            print "COPYING: " + fc + "FROM: " + fgdb
-            fcCopy = fgdb + os.sep + 'Placemarks' + os.sep + fc
-            arcpy.FeatureClassToFeatureClass_conversion(fcCopy, MasterGDBLocation, fgdb[fgdb.rfind(os.sep) + 1: -4])
-            del kmz, wks, fc, featureClasses, fgdb
+    env.overwriteoutput = True
+    arcpy.KMLToLayer_conversion(sys.argv[1], sys.argv[2])
